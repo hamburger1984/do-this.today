411c4fe41347a45a5756bd9a7452831211bcfef6
Andreas K. <hamburger1984@gmail.com>
2025-08-25 16:30:02 +0200

    Adding MIT license

--- app/script.js
+++ app/script.js
@@ -1,3 +1,8 @@
+/*
+ * TaskDice - A mobile-first Progressive Web App for task randomization
+ * Copyright (c) 2025 Andreas Krohn
+ * Licensed under the MIT License. See LICENSE file for details.
+ */
 class TaskRandomizer {
   constructor() {
     this.tasks = [];
