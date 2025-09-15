e482cf4265aea9a496243c60006f0d059e199ee3
Andreas K. <hamburger1984@gmail.com>
2025-09-15 11:32:14 +0200

    Path fixes

--- app/script.js
+++ app/script.js
@@ -2048,7 +2048,7 @@ document.addEventListener("DOMContentLoaded", () => {
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/app/sw.js")
+      .register("/sw.js")
       .then((registration) => {
         console.log("SW registered: ", registration);
       })
